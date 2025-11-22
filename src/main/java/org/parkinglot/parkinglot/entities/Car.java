57256a1d9b5d92a8f8a7211649fb58c57421231f
Flavius <flavius98flo@gmail.com>
2025-11-22 22:33:52 +0200

    Lab 4: Created the EJB

--- src/main/java/org/parkinglot/parkinglot/entities/Car.java
+++ src/main/java/org/parkinglot/parkinglot/entities/Car.java
@@ -16,13 +16,9 @@ public class Car {
     @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
 
-    public User getOwner() {
-        return owner;
-    }
+    public User getOwner() { return owner; }
 
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
+    public void setOwner(User owner) { this.owner = owner; }
 
     public Long getId() {
         return id;
@@ -31,4 +27,9 @@ public class Car {
         this.id = id;
     }
 
+    public String getLicensePlate() { return licensePlate; }
+    public void setLicensePlate(String licensePlate) { this.licensePlate = licensePlate; }
+
+    public String getParkingSpot() { return parkingSpot; }
+    public void setParkingSpot(String parkingSpot) { this.parkingSpot = parkingSpot; }
 }

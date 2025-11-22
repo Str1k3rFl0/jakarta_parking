cab045860e84abeb06a6a679dbfec075ceff3b04
Flavius <flavius98flo@gmail.com>
2025-11-22 23:06:19 +0200

    Lab 4: create "Users" page

---
+++ src/main/java/org/parkinglot/parkinglot/common/UserDto.java
@@ -0,0 +1,20 @@
+package org.parkinglot.parkinglot.common;
+
+public class UserDto {
+    Long id;
+    private String email;
+    private String password;
+    private String username;
+
+    public UserDto(Long id, String email, String password, String username) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Long getId() { return id; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getUsername() { return username; }
+}
---
+++ src/main/java/org/parkinglot/parkinglot/servlets/Users.java
@@ -0,0 +1,31 @@
+package org.parkinglot.parkinglot.servlets;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import org.parkinglot.parkinglot.common.CarDto;
+import org.parkinglot.parkinglot.common.UserDto;
+import org.parkinglot.parkinglot.ejb.UserBean;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Users", value = "/Users")
+public class Users extends HttpServlet {
+
+    @Inject
+    UserBean userBean;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<UserDto> users = userBean.findAllUsers();
+        request.setAttribute("users", users);
+        request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException {
+    }
+}
